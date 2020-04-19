2eaa0fbefbf48f3e31f3fa0cc7d289ba98058919
kaidornel <kai_dornel@hotmail.com>
2020-04-19 09:26:39 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+
+const num1 = 2
+const num2 = 31
+
+let multiply = num1 * num2
+multiply;
+
+const random = Math.ceil(Math.random() * 10)
+
+let mod = 10 % 6;
+
+let max = Math.max(1, 2, 3, 4, 20);
